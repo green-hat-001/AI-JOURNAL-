0628a29121b147622987982f4563b5b80fb880ac
TimmieBanana <93867938+TimmieBanana@users.noreply.github.com>
2025-03-15 11:49:07 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+import google.generativeai as genai
 
+genai.configure(api_key="AIzaSyBHubxpy9MKFDC_uj6fuAh0TqxvJDIGkyc")
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+print (model.generate_content("testing")
